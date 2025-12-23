5f5420c98378a873a4bf816d69772c8f708a2273
bing <spiralladder@fastmail.com>
2025-12-23 21:12:51 +0800

    feat: support time and observeElapsed for `Gauge` and `Histogram` (#1)
    * feat: support time and observeElapsed
    
    Adds support for timing code within `Gauge` and `Histogram` via two new
    fns: `time` and `observeElapsed`
    
    * use std.time.ns_per_s instead
    
    * test: better gauge tests
    
    * test(hist): add timer to noop test
    
    * test(hist): add simple hist test

--- src/gauge.zig
+++ src/gauge.zig
@@ -47,6 +47,20 @@ pub fn Gauge(comptime V: type) type {
             }
         }
 
+        pub fn time(self: *Self) !Impl.Timer {
+            switch (self.*) {
+                .noop => return try Impl.Timer.start(),
+                .impl => |*impl| return impl.time(),
+            }
+        }
+
+        pub fn observeElapsed(self: *Self, timer: *Impl.Timer) !void {
+            switch (self.*) {
+                .noop => {},
+                .impl => |*impl| return try impl.observeElapsed(timer),
+            }
+        }
+
         pub fn write(self: *Self, writer: anytype) !void {
             switch (self.*) {
                 .noop => {},
@@ -57,6 +71,20 @@ pub fn Gauge(comptime V: type) type {
         pub const Impl = struct {
             value: V,
             preamble: []const u8,
+
+            const Timer = struct {
+                inner: std.time.Timer,
+
+                fn start() !Timer {
+                    return .{ .inner = try std.time.Timer.start() };
+                }
+
+                fn read(self: *Timer) f32 {
+                    const ns = self.inner.read();
+                    const secs = @as(f32, @floatFromInt(ns)) / std.time.ns_per_s;
+                    return secs;
+                }
+            };
 
             pub fn init(comptime name: []const u8, comptime opts: Opts) Impl {
                 return .{
@@ -71,6 +99,19 @@ pub fn Gauge(comptime V: type) type {
 
             pub fn incrBy(self: *Impl, value: V) void {
                 _ = @atomicRmw(V, &self.value, .Add, value, .monotonic);
+            }
+
+            pub fn time(_: *Impl) !Timer {
+                return try Timer.start();
+            }
+
+            pub fn observeElapsed(self: *Impl, timer: *Timer) !void {
+                const elapsed = timer.read();
+                switch (@typeInfo(V)) {
+                    .int => self.set(@as(V, @intFromFloat(elapsed))),
+                    .float => self.set(@as(V, @floatCast(elapsed))),
+                    else => @panic("Invalid type"),
+                }
             }
 
             pub fn set(self: *Impl, value: V) void {
@@ -305,6 +346,18 @@ test "Gauge: noop incr/incrBy/set" {
     try t.expectEqual(0, arr.items.len);
 }
 
+test "Gauge: noop time" {
+    // these should just not crash
+    var c = Gauge(u32){ .noop = {} };
+    var timer = try c.time();
+    try c.observeElapsed(&timer);
+
+    var arr = std.ArrayList(u8).init(t.allocator);
+    defer arr.deinit();
+    try c.write(&arr.writer());
+    try t.expectEqual(0, arr.items.len);
+}
+
 test "Gauge: incr/incrBy/set" {
     var g = Gauge(i32).init("t1", .{}, .{});
 
@@ -319,6 +372,27 @@ test "Gauge: incr/incrBy/set" {
 
     g.set(-10);
     try t.expectEqual(-10, g.impl.value);
+}
+
+fn timeAndTest(T: type) !void {
+    var g = Gauge(T).init("t1", .{}, .{});
+    var timer = try g.time();
+    std.Thread.sleep(1);
+
+    _ = try g.observeElapsed(&timer);
+    switch (@typeInfo(T)) {
+        .int => try t.expectEqual(0, g.impl.value), // 1ns is too small to be captured,
+        .float => try std.testing.expect(g.impl.value != 0), // but is captureable by float types
+        else => @compileError("should only test ints and floats for observeElapsed"),
+    }
+}
+
+test "Gauge: time" {
+    try timeAndTest(i32);
+    try timeAndTest(u32);
+    try timeAndTest(u64);
+    try timeAndTest(f32);
+    try timeAndTest(f64);
 }
 
 test "Gauge: write" {
